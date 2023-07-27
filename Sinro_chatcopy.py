08467790c445da7776452182c6a2427a47833c1a
ABHINAV CV <120544834+ABHINAVCV-02@users.noreply.github.com>
2023-07-27 22:16:24 +0530

    Add files via upload

---
+++ Sinro_chatcopy.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = "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"
+
+messages = []
+system_msg = input("What type of chatbot would you like to create?\n")
+messages.append({"role": "system", "content": system_msg})
+
+print("Your new assistant is ready!")
+while input != "quit()":
+    message = input()
+    messages.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+    print("\n" + reply + "\n")
